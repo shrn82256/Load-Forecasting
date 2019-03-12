7d92671629dd67c75a9c935bc7a704f0c715e0df
shrn82256 <shrn82256@gmail.com>
2019-03-12 09:53:42 +0530

    merging nodemcu codes to get data and upload to server

--- nodemcu/nodemcu101.ino
+++ nodemcu/nodemcu101.ino
@@ -1,3 +1,11 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const char *ssid = "abcd";
+const char *password = "abcdefgh";
+
+#define WHITE_LED_PIN 4
+
 bool loadFlag = false;
 bool tempFlag = false;
 
@@ -12,11 +20,24 @@ void setup()
   {
     ; // wait for serial port to connect. Needed for native USB port only
   }
+
+  pinMode(WHITE_LED_PIN, OUTPUT);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+
+    delay(1000);
+    Serial.println("Connecting..");
+    // blink_led(RED_LED_PIN);
+  }
+  Serial.println("Connected");
 }
 
 void loop()
 { // run over and over
-  if (Serial.available())
+  if (Serial.available() && WiFi.status() == WL_CONNECTED)
   {
     char ch = Serial.read();
     if (ch == 'T')
@@ -42,14 +63,46 @@ void loop()
         temp = temp * 10 + ch - '0';
       }
     }
-    else if (ch == '\n')
+    else if (ch == '\n' && temp != 0 && load != 0)
     {
       Serial.print("T:");
       Serial.print(temp);
       Serial.print("\tL:");
       Serial.println(load);
+
+      HTTPClient http;
+
+      String base_url = "http://000.asisodia.com/tarp/";
+      String user = "aryan3";
+
+      http.begin(get_url(base_url, user, temp, load));
+
+      int httpCode = http.GET();
+
+      if (httpCode > 0)
+      {
+        blink_led(WHITE_LED_PIN);
+
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      http.end();
     }
-    // Serial.print(":");
-    // Serial.print();
   }
+}
+
+String get_url(String base_url, String user, float temperature, float load)
+{
+  return base_url + "?user=" + user + "&temperature=" + String(temperature) + "&load=" + String(load);
+}
+
+void blink_led(int ledPinNumber)
+{
+  digitalWrite(ledPinNumber, HIGH);
+  delay(500);
+  digitalWrite(ledPinNumber, LOW);
+  delay(500);
+  digitalWrite(ledPinNumber, HIGH);
+  delay(500);
+  digitalWrite(ledPinNumber, LOW);
 }
