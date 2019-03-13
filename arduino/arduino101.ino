6a5d3ec63b0bd448e9a2500412913b0a1ed7ec20
shrn82256 <shrn82256@gmail.com>
2019-03-14 00:45:58 +0530

    Added 4 led loads

--- arduino/arduino101.ino
+++ arduino/arduino101.ino
@@ -8,8 +8,16 @@ LiquidCrystal lcd(11, 12, 5, 4, 3, 2); */
 #define DHT11_PIN A0
 dht DHT;
 
-int temp, humi;
+int temp;
 String str;
+
+#define N 4
+int led[N] = {2, 3, 4, 5};
+bool led_status[N] = {0, 0, 0, 0};
+
+int load[N] = {8, 9, 10, 11};
+bool load_status[N] = {0, 0, 0, 0};
+int load_value[N] = {1000, 1500, 2000, 2500};
 
 void setup()
 {
@@ -18,19 +26,75 @@ void setup()
 
   Serial.begin(115200);
   mySerial.begin(115200);
+
+  for (int i = 0; i < N; i++)
+  {
+    pinMode(led[i], OUTPUT);
+  }
+
   delay(2000);
 }
+
+int j = 0, k = 1;
 void loop()
 {
+
+  updateLeds(0);
+  updateLed(j, 1);
+  updateLed(k, 1);
+
   DHT.read11(DHT11_PIN);
-  humi = DHT.humidity;
   temp = DHT.temperature;
-  str = String('T') + String(temp) + String('L') + String(humi * 10);
+  str = String('T') + String(temp) + String('L') + String(getLoad());
+
   Serial.println(str);
   mySerial.println(str);
 
   /* lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(str); */
+
+  j = (j + 1) % N;
+  k = (k + 1) % N;
   delay(5000);
+}
+
+void updateLed(int i, bool status)
+{
+  if (status)
+    digitalWrite(led[i], HIGH);
+  else
+    digitalWrite(led[i], LOW);
+
+  led_status[i] = status;
+}
+
+void updateLeds(bool status)
+{
+  for (int i = 0; i < N; i++)
+  {
+    if (status)
+      digitalWrite(led[i], HIGH);
+    else
+      digitalWrite(led[i], LOW);
+  }
+}
+
+void updateLoads()
+{
+  for (int i = 0; i < N; i++)
+    load_status[i] = digitalRead(load[i]);
+}
+
+int getLoad()
+{
+  updateLoads();
+
+  int total_load = 0;
+
+  for (int i = 0; i < N; i++)
+    if (load_status[i])
+      total_load += load_value[i];
+
+  return total_load;
 }
